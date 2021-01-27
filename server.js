e4f874aea4a7a6dd2a101f01e522505faee25ace
William Bryan <wwbryan00@gmail.com>
2021-01-26 20:34:32 -0800

    add paths

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -6,10 +7,14 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
-  
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
