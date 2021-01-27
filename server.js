a0b60a4a14dc0d1ba971ee9e62aebd922e1fe65f
William Bryan <wwbryan00@gmail.com>
2021-01-26 22:06:10 -0800

    add listener to save button that posts

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+var notes = [];
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
@@ -17,4 +20,8 @@ app.get("/", function(req, res) {
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    return res.json(notes);
 });
